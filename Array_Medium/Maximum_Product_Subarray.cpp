e7355cfaed5817f3fc8d7cb09b39ee1eacc2f202
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-09-07 00:20:20 +0530

    Maximum Product Subarray

---
+++ Array_Medium/Maximum_Product_Subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int leftproduct=1;
+        int rightproduct=1;
+        int ans=nums[0];
+        for(int i=0;i<n;i++)
+        {
+            if(leftproduct==0){
+                leftproduct=1;
+                
+            }
+            if(rightproduct==0){
+                rightproduct=1;
+                
+            }
+
+
+           leftproduct=leftproduct*nums[i];
+           rightproduct=rightproduct*nums[n-1-i];
+           ans=max(ans,max(leftproduct,rightproduct));
+
+        }
+        return ans;
+    }
+};
