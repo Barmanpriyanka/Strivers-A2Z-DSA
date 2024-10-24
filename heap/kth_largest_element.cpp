2b4388bd8718e10244dd788451bc60c2755cb73d
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-10-25 00:53:17 +0530

    second approach to find the kth largest element in an array .

--- heap/kth_largest_element.cpp
+++ heap/kth_largest_element.cpp
@@ -12,3 +12,23 @@ public:
         return ans;
     }
 };
+
+
+//second approach
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+
+        for(auto x:nums){
+            pq.push(x);
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+            
+        }
+        return pq.top();
+    }
+
+};
