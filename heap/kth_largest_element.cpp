14564e6633849532dd7071b109bd0608691e5f6a
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-11-30 11:14:51 +0530

    largest kth element .

--- heap/kth_largest_element.cpp
+++ heap/kth_largest_element.cpp
@@ -32,3 +32,29 @@ public:
     }
 
 };
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        //step 1 
+         priority_queue<int,vector<int>,greater<int>>pq;
+         for(int i=0;i<k;i++)
+         {
+             pq.push(nums[i]);
+         }
+         //step2  for remaing element after k th element 
+         for(int i=k;i<n;i++)
+         {
+             if(nums[i]>pq.top())
+             {
+                 pq.pop();
+                 pq.push(nums[i]);
+             }
+         }
+         return  pq.top();
+         
+    }
+};
+         
