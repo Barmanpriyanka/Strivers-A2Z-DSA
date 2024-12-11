e98fa14c161c0de9efb9b2e64b1b17141e896058
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-12-12 00:22:14 +0530

    second approach using heap.

--- heap/kth_largest_sum_subarray.cpp
+++ heap/kth_largest_sum_subarray.cpp
@@ -53,3 +53,35 @@ int main() {
 }
 
 // } Driver Code Ends
+
+
+
+
+
+
+
+// approach 2
+ int kthLargest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>> mini;
+        int n= arr.size();
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=i;j<n;j++)
+            {
+                sum=sum+arr[j];
+                if(mini.size()< k)
+                {
+                    mini.push(sum);
+                }
+                else {
+                    if(sum > mini.top()){
+                        mini.pop();
+                        mini.push(sum);
+                    }
+                }
+            }
+        }
+        return mini.top();
+    }
