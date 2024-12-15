eed922c2ba3ded1a34f9c34f9b66b62994ac34fc
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-12-15 12:05:34 +0530

    Print 1 to  N without loop.

---
+++ Recursion/Print_1toN.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // Complete this function
+    void printNos(int n) {
+        // Your code here
+        if(n==0){
+            return ;
+        }
+        printNos(n-1);
+        cout<<n<<" ";
+        
+    }
+};
