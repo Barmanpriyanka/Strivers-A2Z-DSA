3a75fdbb24493a2e3ae0ec54f35141e8267216a6
Barmanpriyanka <priyankabarman1604@gmail.com>
2025-07-31 15:07:37 +0530

    Optimal aproach .

--- sliding_window/Binary_Subarrays_Sum.cpp
+++ sliding_window/Binary_Subarrays_Sum.cpp
@@ -21,3 +21,40 @@ public:
         return count;
     }
 };
+
+
+//another and optimal aproach 
+
+class Solution {
+    int solve(vector<int>& nums,int goal)
+    {
+        int n=nums.size();
+        int sum=0;
+        int count=0;
+        int left=0;
+
+        if(goal<0){
+        return 0;}
+
+        for(int right=0;right<n;right++)
+        {
+            sum=sum+nums[right];
+          
+            while(sum > goal)
+            {
+                 sum=sum-nums[left];
+                left++;
+            }
+
+            count =count+(right-left+1);
+        }
+
+        return count;
+
+    }
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return solve(nums,goal)-solve(nums,goal-1);
+    }
+
+};
