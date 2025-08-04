83563f04a37f3859f3bd22d9ef3c416398e31736
Barmanpriyanka <priyankabarman1604@gmail.com>
2025-08-04 23:25:47 +0530

    Fruit Into Baskets.

---
+++ sliding_window/Fruit_Into_Baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        int left=0;
+        int maxfruits=0;
+        unordered_map<int,int> basket;
+        for(int right=0;right<n;right++)
+        {
+            basket[fruits[right]]++;
+
+            while(basket.size()>2)
+            {
+                basket[fruits[left]]--;
+                if(basket[fruits[left]]==0)
+                {
+                    basket.erase(fruits[left]);
+                }
+                left++;
+            }
+
+              maxfruits=max(maxfruits,right-left+1);
+
+        }
+        return maxfruits;
+    }
+};
